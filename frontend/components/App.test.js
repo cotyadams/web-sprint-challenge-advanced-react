5e4be153a44846b2c3c084f3f3df4e6511ae1968
cotyadams <cotyadams1@gmail.com>
2024-01-03 12:33:30 -0500

    created rtl tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,31 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+import AppClass from './AppClass';
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import React from "react";
+test('loads webpage', () => {
+  render(<AppClass />);
 })
+test('left appears', () => {
+  render(<AppClass />);
+  //left
+  const left = screen.getByText('LEFT');
+  expect(left).toBeInTheDocument();
+})
+test('right appears', () => {
+  render(<AppClass />);
+  //right
+  const right = screen.getByText('RIGHT');
+  expect(right).toBeInTheDocument();
+})
+test('up appears', () => {
+  render(<AppClass />);
+  const up = screen.getByText('UP');
+  expect(up).toBeInTheDocument();
+})
+test('down appears', () => {
+  render(<AppClass />);
+  const down = screen.getByText('DOWN');
+  expect(down).toBeInTheDocument();
+})
+
